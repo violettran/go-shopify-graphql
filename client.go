b1c9ffe33fdc820a996a7275e4ea6949c268e443
leon.vu <leon.vu@gempages.help>
2022-09-26 15:15:59 +0700

    Update shopify version

--- client.go
+++ client.go
@@ -1,125 +1,125 @@
 package shopify
 
 import (
-	"os"
+    "os"
 
-	graphqlclient "github.com/es-hs/go-shopify-graphql/graph"
-	"github.com/es-hs/go-shopify-graphql/graphql"
+    graphqlclient "github.com/es-hs/go-shopify-graphql/graph"
+    "github.com/es-hs/go-shopify-graphql/graphql"
 
-	log "github.com/sirupsen/logrus"
+    log "github.com/sirupsen/logrus"
 )
 
 const (
-	shopifyAPIVersion           = "2021-01"
-	shopifyStoreFrontAPIVersion = "2022-01"
+    shopifyAPIVersion           = "2022-07"
+    shopifyStoreFrontAPIVersion = "2022-07"
 )
 
 type Client struct {
-	gql *graphql.Client
+    gql *graphql.Client
 
-	Product       ProductService
-	Variant       VariantService
-	Inventory     InventoryService
-	Collection    CollectionService
-	Cart          CartService
-	Order         OrderService
-	Fulfillment   FulfillmentService
-	Location      LocationService
-	Metafield     MetafieldService
-	BulkOperation BulkOperationService
-	Webhook       WebhookService
+    Product       ProductService
+    Variant       VariantService
+    Inventory     InventoryService
+    Collection    CollectionService
+    Cart          CartService
+    Order         OrderService
+    Fulfillment   FulfillmentService
+    Location      LocationService
+    Metafield     MetafieldService
+    BulkOperation BulkOperationService
+    Webhook       WebhookService
 }
 
 type ListOptions struct {
-	Query   string
-	First   int
-	Last    int
-	After   string
-	Before  string
-	Reverse bool
+    Query   string
+    First   int
+    Last    int
+    After   string
+    Before  string
+    Reverse bool
 }
 
 func NewDefaultClient() (shopClient *Client) {
-	apiKey := os.Getenv("STORE_API_KEY")
-	password := os.Getenv("STORE_PASSWORD")
-	storeName := os.Getenv("STORE_NAME")
-	if apiKey == "" || password == "" || storeName == "" {
-		log.Fatalln("Shopify app API Key and/or Password and/or Store Name not set")
-	}
+    apiKey := os.Getenv("STORE_API_KEY")
+    password := os.Getenv("STORE_PASSWORD")
+    storeName := os.Getenv("STORE_NAME")
+    if apiKey == "" || password == "" || storeName == "" {
+        log.Fatalln("Shopify app API Key and/or Password and/or Store Name not set")
+    }
 
-	shopClient = NewClient(apiKey, password, storeName)
+    shopClient = NewClient(apiKey, password, storeName)
 
-	return
+    return
 }
 
 func NewClient(apiKey string, password string, storeName string) *Client {
-	c := &Client{gql: newShopifyGraphQLClient(apiKey, password, storeName)}
+    c := &Client{gql: newShopifyGraphQLClient(apiKey, password, storeName)}
 
-	c.Product = &ProductServiceOp{client: c}
-	c.Variant = &VariantServiceOp{client: c}
-	c.Inventory = &InventoryServiceOp{client: c}
-	c.Cart = &CartServiceOp{client: c}
-	c.Collection = &CollectionServiceOp{client: c}
-	c.Order = &OrderServiceOp{client: c}
-	c.Fulfillment = &FulfillmentServiceOp{client: c}
-	c.Location = &LocationServiceOp{client: c}
-	c.Metafield = &MetafieldServiceOp{client: c}
-	c.BulkOperation = &BulkOperationServiceOp{client: c}
-	c.Webhook = &WebhookServiceOp{client: c}
+    c.Product = &ProductServiceOp{client: c}
+    c.Variant = &VariantServiceOp{client: c}
+    c.Inventory = &InventoryServiceOp{client: c}
+    c.Cart = &CartServiceOp{client: c}
+    c.Collection = &CollectionServiceOp{client: c}
+    c.Order = &OrderServiceOp{client: c}
+    c.Fulfillment = &FulfillmentServiceOp{client: c}
+    c.Location = &LocationServiceOp{client: c}
+    c.Metafield = &MetafieldServiceOp{client: c}
+    c.BulkOperation = &BulkOperationServiceOp{client: c}
+    c.Webhook = &WebhookServiceOp{client: c}
 
-	return c
+    return c
 }
 
 func newShopifyGraphQLClient(apiKey string, password string, storeName string) *graphql.Client {
-	opts := []graphqlclient.Option{
-		graphqlclient.WithVersion(shopifyAPIVersion),
-		graphqlclient.WithPrivateAppAuth(apiKey, password),
-	}
-	return graphqlclient.NewClient(storeName, opts...)
+    opts := []graphqlclient.Option{
+        graphqlclient.WithVersion(shopifyAPIVersion),
+        graphqlclient.WithPrivateAppAuth(apiKey, password),
+    }
+    return graphqlclient.NewClient(storeName, opts...)
 }
 
 func (c *Client) GraphQLClient() *graphql.Client {
-	return c.gql
+    return c.gql
 }
 
 func NewClientWithToken(apiKey string, storeName string) *Client {
-	c := &Client{gql: newShopifyGraphQLClientWithToken(apiKey, storeName)}
+    c := &Client{gql: newShopifyGraphQLClientWithToken(apiKey, storeName)}
 
-	c.Product = &ProductServiceOp{client: c}
-	c.Variant = &VariantServiceOp{client: c}
-	// c.Inventory = &InventoryServiceOp{client: c}
-	c.Cart = &CartServiceOp{client: c}
-	c.Collection = &CollectionServiceOp{client: c}
-	// c.Order = &OrderServiceOp{client: c}
-	// c.Fulfillment = &FulfillmentServiceOp{client: c}
-	// c.Location = &LocationServiceOp{client: c}
-	c.Metafield = &MetafieldServiceOp{client: c}
-	c.BulkOperation = &BulkOperationServiceOp{client: c}
-	c.Webhook = &WebhookServiceOp{client: c}
+    c.Product = &ProductServiceOp{client: c}
+    c.Variant = &VariantServiceOp{client: c}
+    // c.Inventory = &InventoryServiceOp{client: c}
+    c.Cart = &CartServiceOp{client: c}
+    c.Collection = &CollectionServiceOp{client: c}
+    // c.Order = &OrderServiceOp{client: c}
+    // c.Fulfillment = &FulfillmentServiceOp{client: c}
+    // c.Location = &LocationServiceOp{client: c}
+    c.Metafield = &MetafieldServiceOp{client: c}
+    c.BulkOperation = &BulkOperationServiceOp{client: c}
+    c.Webhook = &WebhookServiceOp{client: c}
 
-	return c
+    return c
 }
 
 func NewClientStoreFrontWithToken(apiKey string, storeName string) *Client {
-	c := &Client{gql: newShopifyStoreFrontGraphQLClientWithToken(apiKey, storeName)}
-	c.Cart = &CartServiceOp{client: c}
-	return c
+    c := &Client{gql: newShopifyStoreFrontGraphQLClientWithToken(apiKey, storeName)}
+    c.Cart = &CartServiceOp{client: c}
+    return c
 }
 
 func newShopifyGraphQLClientWithToken(token string, storeName string) *graphql.Client {
-	opts := []graphqlclient.Option{
-		graphqlclient.WithVersion(shopifyAPIVersion),
-		graphqlclient.WithToken(token),
-	}
-	// todo no more fixed storeName
-	return graphqlclient.NewClient(storeName, opts...)
+    opts := []graphqlclient.Option{
+        graphqlclient.WithVersion(shopifyAPIVersion),
+        graphqlclient.WithToken(token),
+    }
+    // todo no more fixed storeName
+    return graphqlclient.NewClient(storeName, opts...)
 }
 
 func newShopifyStoreFrontGraphQLClientWithToken(token string, storeName string) *graphql.Client {
-	opts := []graphqlclient.Option{
-		graphqlclient.WithStoreFrontVersion(shopifyStoreFrontAPIVersion),
-		graphqlclient.WithStoreFrontToken(token),
-	}
-	// todo no more fixed storeName
-	return graphqlclient.NewClient(storeName, opts...)
+    opts := []graphqlclient.Option{
+        graphqlclient.WithStoreFrontVersion(shopifyStoreFrontAPIVersion),
+        graphqlclient.WithStoreFrontToken(token),
+    }
+    // todo no more fixed storeName
+    return graphqlclient.NewClient(storeName, opts...)
 }
