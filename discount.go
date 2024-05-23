fc71152c2de6852f1210107eb478a47042821957
Violet Tran <148740322+violettran@users.noreply.github.com>
2024-05-23 18:13:48 +0700

    feat: create/update automatic app discount (#86)

---
+++ discount.go
@@ -0,0 +1,120 @@
+package shopify
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/gempages/go-shopify-graphql-model/graph/model"
+)
+
+type DiscountService interface {
+	AutomaticAppCreate(ctx context.Context, discount model.DiscountAutomaticAppInput) (output *model.DiscountAutomaticApp, err error)
+	AutomaticAppUpdate(ctx context.Context, discountBaseID string, discount model.DiscountAutomaticAppInput) (output *model.DiscountAutomaticApp, err error)
+}
+
+type DiscountServiceOp struct {
+	client *Client
+}
+
+var _ DiscountService = &DiscountServiceOp{}
+
+type mutationDiscountAutomaticAppCreate struct {
+	DiscountAutomaticCreateAppPayload model.DiscountAutomaticAppCreatePayload `json:"discountAutomaticCreateAppPayload"`
+}
+
+type mutationDiscountAutomaticAppUpdate struct {
+	DiscountAutomaticAppUpdatePayload model.DiscountAutomaticAppUpdatePayload `json:"discountAutomaticAppUpdatePayload"`
+}
+
+var discountAutomaticAppCreate = `
+mutation discountAutomaticAppCreate($automaticAppDiscount: DiscountAutomaticAppInput!) {
+  discountAutomaticAppCreate(automaticAppDiscount: $automaticAppDiscount) {
+    userErrors {
+      field
+      message
+    }
+    automaticAppDiscount {
+      discountId
+      title
+      startsAt
+      endsAt
+      status
+      appDiscountType {
+        appKey
+        functionId
+      }
+      combinesWith {
+        orderDiscounts
+        productDiscounts
+        shippingDiscounts
+      }
+    }
+  }
+}
+`
+
+var discountAutomaticAppUpdate = `
+mutation discountAutomaticAppUpdate($automaticAppDiscount: DiscountAutomaticAppInput!, $id: ID!) {
+  discountAutomaticAppUpdate(automaticAppDiscount: $automaticAppDiscount, id: $id) {
+    automaticAppDiscount {
+      discountId
+      title
+      startsAt
+      endsAt
+      status
+      appDiscountType {
+        appKey
+        functionId
+      }
+      combinesWith {
+        orderDiscounts
+        productDiscounts
+        shippingDiscounts
+      }
+    }
+    userErrors {
+      field
+      message
+    }
+  }
+}
+`
+
+func (s *DiscountServiceOp) AutomaticAppCreate(ctx context.Context, input model.DiscountAutomaticAppInput) (output *model.DiscountAutomaticApp, err error) {
+	out := mutationDiscountAutomaticAppCreate{}
+	vars := map[string]interface{}{
+		"title":        input.Title,
+		"functionId":   input.FunctionID,
+		"startsAt":     input.StartsAt,
+		"endsAt":       input.EndsAt,
+		"combinesWith": input.CombinesWith,
+		"metafields":   input.Metafields,
+	}
+
+	if err := s.client.gql.MutateString(ctx, discountAutomaticAppCreate, vars, &out); err != nil {
+		return nil, fmt.Errorf("gql.MutateString: %w", err)
+	}
+
+	if len(out.DiscountAutomaticCreateAppPayload.UserErrors) > 0 {
+		return nil, fmt.Errorf("%+v", out.DiscountAutomaticCreateAppPayload.UserErrors)
+	}
+
+	return out.DiscountAutomaticCreateAppPayload.AutomaticAppDiscount, nil
+}
+
+func (s *DiscountServiceOp) AutomaticAppUpdate(ctx context.Context, discountBaseID string, input model.DiscountAutomaticAppInput) (output *model.DiscountAutomaticApp, err error) {
+	out := mutationDiscountAutomaticAppUpdate{}
+	vars := map[string]interface{}{
+		"id":                   discountBaseID,
+		"automaticAppDiscount": input,
+	}
+	if err := s.client.gql.MutateString(ctx, discountAutomaticAppUpdate, vars, &out); err != nil {
+		return nil, fmt.Errorf("gql.MutateString: %w", err)
+	}
+
+	if len(out.DiscountAutomaticAppUpdatePayload.UserErrors) > 0 {
+		return nil, fmt.Errorf("%+v", out.DiscountAutomaticAppUpdatePayload.UserErrors)
+	}
+
+	return out.DiscountAutomaticAppUpdatePayload.AutomaticAppDiscount, nil
+}
