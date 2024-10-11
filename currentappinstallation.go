f1fbc9b6c314f15307e8a092b2480559368d933b
Violet Tran <148740322+violettran@users.noreply.github.com>
2024-10-11 16:55:41 +0700

    86cwau15y - feat: find active subscriptions (#115)

--- currentappinstallation.go
+++ currentappinstallation.go
@@ -9,6 +9,7 @@ import (
 
 type AppService interface {
 	GetCurrentAppInstallation(ctx context.Context) (*model.AppInstallation, error)
+	FindActiveAppSubscriptions(ctx context.Context) ([]model.AppSubscription, error)
 }
 
 type AppServiceOp struct {
@@ -57,4 +58,34 @@ func (a *AppServiceOp) GetCurrentAppInstallation(ctx context.Context) (*model.Ap
 	}
 
 	return out.CurrentAppInstallation, nil
+}
+
+var queryActiveSubscriptions = fmt.Sprintf(`
+query {
+	currentAppInstallation {
+		activeSubscriptions {
+			createdAt
+			currentPeriodEnd
+			id
+			name
+			returnUrl
+			status
+			test
+			trialDays
+		}
+	}
+}
+`)
+
+func (a *AppServiceOp) FindActiveAppSubscriptions(ctx context.Context) ([]model.AppSubscription, error) {
+	out := struct {
+		CurrentAppInstallation *model.AppInstallation `json:"currentAppInstallation"`
+	}{}
+
+	err := a.client.gql.QueryString(ctx, queryActiveSubscriptions, nil, &out)
+	if err != nil {
+		return nil, err
+	}
+
+	return out.CurrentAppInstallation.ActiveSubscriptions, nil
 }
